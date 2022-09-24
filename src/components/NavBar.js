b38c83367de541d1466645a3994f3d5323990e12
Gonzalo <gonzaleone03@gmail.com>
2022-09-24 19:16:49 -0300

    otro commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import CartWidget from './CartWidget'
+
+function NavBar() {
+    return (
+        <div>
+            <h1>GL INDUMENTARIA</h1>
+            
+
+            <CartWidget />              
+        </div>
+
+    
+        
+    )
+}
+
+export default NavBar
