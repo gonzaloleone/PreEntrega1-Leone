214812c7830a36644629d9f865282d6d2e84dc64
Gonzalo <gonzaleone03@gmail.com>
2022-09-25 19:17:23 -0300

    otro commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,31 @@
-import CartWidget from './CartWidget'
+import CartWidget from './CartWidget';
+import './componente.css';
 
 function NavBar() {
     return (
-        <div>
+        <div className='contenedor'>
             <h1>GL INDUMENTARIA</h1>
-            
+            <nav className="navBar">
+            <ul className= "navUl">
+                <li className= "navLi">
+                    <a className= "navLink" href="">INICIO</a>
+                </li>
+                <li className= "navLi">
+                    <a className= "navLink" href="">PRODUCTOS</a>
+                </li>
+                <li className= "navLi">
+                    <a className= "navLink" href="">PREGUNTAS</a>
+                </li>
+                <li className= "navLi">
+                    <a className= "navLink" href="">CONTACTO</a>
+                </li>
+                <li className= "navLi">
+                    <a className= "navLink" href=""><CartWidget/></a>
+                </li>
+            </ul>
+        </nav>
 
-            <CartWidget />              
+                        
         </div>
 
     
